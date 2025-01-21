ab9721b53b1dd1380ea62c4b91ba1443aca680e6
Valentina Osorio <va.os.consulting@gmail.com>
2025-01-21 18:19:51 +0000

    first few steps of setting up server

--- server/server.ts
+++ server/server.ts
@@ -12,6 +12,30 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
+
+app.get('/', async (req, res, next) => {
+  console.log('/api/entries hit');
+  try {
+    const sql = `
+    select "entryId", "notes", "title", "photoUrl"
+    from "entries"
+    `;
+
+    const result = await db.query(sql);
+    console.log(result.rows);
+    const entries = result.rows;
+
+    if (!entries) {
+      throw new ClientError(400, `No entries found`);
+    }
+    res.json(entries);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
