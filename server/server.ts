3668891a0ecef3d0c95f24270c77bd326a6e1546
Valentina Osorio <va.os.consulting@gmail.com>
2025-01-21 19:01:01 +0000

    progress on server

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
+import { ClientRequest } from 'http';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -18,8 +19,7 @@ app.get('/api/entries', async (req, res, next) => {
   console.log('/api/entries hit');
   try {
     const sql = `
-    select "entryId", "notes", "title", "photoUrl"
-    from "entries"
+    select * from "entries" order by "entryId" desc;
     `;
 
     const result = await db.query(sql);
@@ -30,6 +30,36 @@ app.get('/api/entries', async (req, res, next) => {
       throw new ClientError(400, `No entries found`);
     }
     res.json(entries);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('api/entries/:entryId', async (req, res, next) => {
+  console.log('api/entries/:entryId hit');
+  try {
+    const { entryId } = req.params;
+    console.log(entryId);
+
+    if (!Number.isInteger(+entryId)) {
+      throw new ClientError(400, `Non-integer entryId: ${entryId}`);
+    }
+
+    const sql = `
+    select *
+    from "entries"
+    where "entryId" = $1
+    `;
+
+    const params = [entryId];
+    const result = await db.query(sql, params);
+    console.log(result.rows);
+    const entry = result.rows[0];
+
+    if (!entry) {
+      throw new ClientError(400, `entryId ${entryId} not found`);
+    }
+    res.json(entry);
   } catch (err) {
     next(err);
   }
