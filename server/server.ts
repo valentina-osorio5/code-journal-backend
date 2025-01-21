b33b8a8a2d48baade88982f20aa2482034b325d6
TreMitchell <trem963@gmail.com>
2025-01-21 20:07:39 +0000

    Server Routes completed.

--- server/server.ts
+++ server/server.ts
@@ -35,10 +35,10 @@ app.get('/api/entries', async (req, res, next) => {
   }
 });
 
-app.get('api/entries/:entryId', async (req, res, next) => {
-  console.log('api/entries/:entryId hit');
+app.get('/api/entries/:entryId', async (req, res, next) => {
+  console.log('/api/entries/:entryId hit');
   try {
-    const entryId = req.params.entryId;
+    const entryId = Number(req.params.entryId);
     console.log(entryId);
     if (!Number.isInteger(+entryId)) {
       throw new ClientError(400, `Non-integer entryId: ${entryId}`);
@@ -47,13 +47,13 @@ app.get('api/entries/:entryId', async (req, res, next) => {
     const sql = `
     select *
     from "entries"
-    where "entryId" = $1
+    where "entryId" = $1;
     `;
 
     const params = [entryId];
     const result = await db.query(sql, params);
     console.log(result.rows);
-    const entry = result.rows[0];
+    const [entry] = result.rows;
 
     if (!entry) {
       throw new ClientError(400, `entryId ${entryId} not found`);
@@ -80,6 +80,59 @@ app.post('/api/new-entry', async (req, res, next) => {
     const result = await db.query<Entry>(sql, params);
     const newEntry = result.rows[0];
     res.json(newEntry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/entries/:entryId', async (req, res, next) => {
+  console.log('/api/entries/:entryId update hit!');
+  try {
+    const { entryId } = req.params;
+    if (!Number.isInteger(+entryId)) {
+      throw new ClientError(400, `Non-integer entryId: ${entryId}`);
+    }
+
+    const { title, notes, photoUrl } = req.body;
+    const sql = `
+    update "entries"
+    set "title" = $1, "notes" = $2, "photoUrl" = $3
+    where "entryId" = $4
+    returning *;
+    `;
+
+    const params = [title, notes, photoUrl, entryId];
+    const result = await db.query(sql, params);
+    console.log(result.rows);
+    const updateEntry = result.rows[0];
+    if (!updateEntry) {
+      throw new ClientError(404, ` ${entryId} doesn't exist`);
+    }
+    res.json(updateEntry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/api/entries/:entryId', async (req, res, next) => {
+  console.log('/api/entries/:entryId delete hit');
+
+  try {
+    const { entryId } = req.params;
+    if (!Number.isInteger(+entryId)) {
+      throw new ClientError(400, `Non-integer entryId: ${entryId}`);
+    }
+    const sql = `
+    delete from "entries" where "entryId" = $1 returning *;
+    `;
+    const params = [entryId];
+    const result = await db.query(sql, params);
+    console.log(result.rows);
+    const deleteEntry = result.rows[0];
+    if (!deleteEntry) {
+      throw new ClientError(404, `entry ${entryId} doesn't exist!`);
+    }
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
