2bd82d843484a25809b290b88b9691c3be81a599
Valentina Osorio <va.os.consulting@gmail.com>
2025-01-21 18:08:05 +0000

    starter code

--- server/server.ts
+++ server/server.ts
@@ -12,6 +12,7 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
